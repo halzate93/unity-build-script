e30bbb4c8ca28a8527c46911179ae530cc37edbd
Héctor C. Alzate <halzate93@gmail.com>
2016-10-19 09:02:36 -0500

    Removed verbose

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ var config = require ('config')
 
 // load the configuration file
 var settings = config.get ('cloudbuild')
-console.log ("settings: " + JSON.stringify (settings))
+// console.log ("settings: " + JSON.stringify (settings))
 
 // construct request url which identifies the resource to build
 var url = `${settings.domain}/orgs/${settings.org_id}/projects/`
         + `${settings.project_id}/buildtargets/${settings.build_target_id}/builds`
-console.log ("url: " + url)
+// console.log ("url: " + url)
 
 // construct headers with authentication
 var headers = {
   "Authorization": "Basic " + settings.api_key,
   "Content-Type": "application/json",
 }
-console.log ("headers: " + JSON.stringify (headers))
+// console.log ("headers: " + JSON.stringify (headers))
 
 // construct the request body with build parameters
 var body = {
   "clean": settings.clean,
   "delay": settings.delay
 }
-console.log ("body: " + JSON.stringify (body))
+// console.log ("body: " + JSON.stringify (body))
 
 // make ajax request to the api
 request.post (url)
@@ -34,7 +34,13 @@ request.post (url)
   .end (function (error, response) // send request
   {
     if (error)
-      console.log ("There was a problem with the request:\n${error}", error)
+    {
+      console.log ("There was a problem with the request")
+      // console.log (JSON.stringify (error))
+    }
     else
-      console.log ("Build has been started successfully:\n${response}", response)
+    {
+      console.log ("Build has been started successfully")
+      // console.log (JSON.stringify (response))
+    }
   })
